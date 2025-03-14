4874ace6f202a9704e04312ac1645f037a1ed0bd
AniiitaCode <anita_nen4eva@abv.bg>
2025-03-14 15:55:40 +0200

    Create ExceptionAdvice - general error

---
+++ src/main/java/com/example/art/web/ExceptionAdvice.java
@@ -0,0 +1,21 @@
+package com.example.art.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ExceptionAdvice {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleAnyException(Exception exception) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("server-error");
+
+        return modelAndView;
+    }
+}
