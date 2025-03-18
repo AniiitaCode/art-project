813075ca38d9f506b1aedf52b527388708725db1
AniiitaCode <anita_nen4eva@abv.bg>
2025-03-18 19:22:47 +0200

    User Service Testing

--- src/test/java/com/example/art/UserServiceUTest.java
+++ src/test/java/com/example/art/UserServiceUTest.java
@@ -3,18 +3,28 @@ package com.example.art;
 import com.example.art.email.service.EmailService;
 import com.example.art.exception.DomainException;
 import com.example.art.exception.UsernameAlreadyExistException;
+import com.example.art.security.AuthenticationDetails;
 import com.example.art.user.model.User;
 import com.example.art.user.model.UserRole;
 import com.example.art.user.repository.UserRepository;
 import com.example.art.user.service.UserService;
+import com.example.art.wallet.service.WalletService;
 import com.example.art.web.dto.RegisterRequest;
+import com.example.art.web.dto.UserEditRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -28,21 +38,37 @@ public class UserServiceUTest {
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private WalletService walletService;
+
+    @Mock
+    private EmailService emailService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Captor
+    private ArgumentCaptor<User> userCaptor;
+
     @InjectMocks
     private UserService userService;
 
     @Test
-    public void testSwitchRole_userNotFound() {
+     void whenSwitchRole_thenUserNotFound() {
 
         UUID userId = UUID.randomUUID();
-        when(userRepository.findById(userId)).thenReturn(null);
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
 
-        assertThrows(NullPointerException.class, () -> userService.switchRole(userId));
+       DomainException exception =
+               assertThrows(DomainException.class, () -> userService.switchRole(userId));
+
+       assertEquals("User with id [%s] does not exist.".formatted(userId),
+               exception.getMessage());
     }
 
 
     @Test
-    public void testSwitchRole_userIsAlreadyAdmin() {
+     void whenSwitchRole_thenUserIsAlreadyAdmin() {
 
         UUID userId = UUID.randomUUID();
         User existingUser = User.builder()
@@ -50,14 +76,17 @@ public class UserServiceUTest {
                 .role(UserRole.ADMIN)
                 .build();
 
-        when(userRepository.findById(userId)).thenReturn(Optional.ofNullable(existingUser));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
 
-        assertThrows(DomainException.class, () -> userService.switchRole(userId));
+        DomainException exception =
+                assertThrows(DomainException.class, () -> userService.switchRole(userId));
+
+        assertEquals("This user is already an admin!", exception.getMessage());
     }
 
 
     @Test
-    public void testSwitchRole_Success() {
+     void whenSwitchRole_thenSuccess() {
 
         UUID userId = UUID.randomUUID();
         User normalUser = User.builder()
@@ -74,11 +103,11 @@ public class UserServiceUTest {
 
 
     @Test
-    public void testRegister_userAlreadyExist() {
-        RegisterRequest registerRequest = new RegisterRequest();
-        registerRequest.setUsername("user");
-        registerRequest.setEmail("existing_user@example.com");
-        registerRequest.setPassword("123123");
+     void whenRegister_thenUserAlreadyExist() {
+        RegisterRequest dto = new RegisterRequest();
+        dto.setUsername("user");
+        dto.setEmail("existing_user@example.com");
+        dto.setPassword("123123");
 
         User excitingUser = User.builder()
                 .username("user")
@@ -90,11 +119,194 @@ public class UserServiceUTest {
                 .thenReturn(Optional.of(excitingUser));
 
         UsernameAlreadyExistException exception = assertThrows(UsernameAlreadyExistException.class, ()
-        -> userService.register(registerRequest));
+        -> userService.register(dto));
 
         assertEquals("This username or email already exist.", exception.getMessage());
+        verify(userRepository, never()).save(any());
+        verify(walletService, never()).createWallet(any());
+        verify(emailService, never()).savePreference(any(UUID.class), anyBoolean(), anyString());
+    }
+
+
+    @Test
+    void happyPath_whenRegister_thenUserRegisterAndSaveDatabase_withRoleAdmin() {
+
+        RegisterRequest dto = new RegisterRequest();
+        dto.setUsername("user");
+        dto.setFirstName("User");
+        dto.setPassword("123123");
+        dto.setConfirmPassword("123123");
+        dto.setEmail("user@abv.bg");
+
+        User user = User.builder()
+                .id(UUID.randomUUID())
+                .username(dto.getUsername())
+                .email(dto.getEmail())
+                .firstName(dto.getFirstName())
+                .password(dto.getPassword())
+                .build();
+
+        lenient().when(userRepository.findByUsernameOrEmail(dto.getUsername(), dto.getEmail())).thenReturn(Optional.empty());
+        lenient().when(userRepository.save(any())).thenReturn(user);
+        lenient().when(userRepository.count()).thenReturn(0L);
+
+        userService.register(dto);
+
+        verify(userRepository, times(1)).save(userCaptor.capture());
+
+        User registeredUser = userCaptor.getValue();
+
+        assertEquals("user", registeredUser.getUsername());
+        assertEquals("User", registeredUser.getFirstName());
+        assertEquals("user@abv.bg", registeredUser.getEmail());
+
+
+        assertEquals(UserRole.ADMIN, registeredUser.getRole());
+
+
+        verify(emailService, times(1))
+                .savePreference(registeredUser.getId(), true, registeredUser.getEmail());
+        verify(walletService, times(1))
+                .createWallet(registeredUser);
+    }
+
+
+    @Test
+    void happyPath_whenRegister_thenUserRegisterAndSaveDatabase_withRoleUser() {
+
+        RegisterRequest dto = new RegisterRequest();
+        dto.setUsername("user");
+        dto.setFirstName("User");
+        dto.setPassword("123123");
+        dto.setConfirmPassword("123123");
+        dto.setEmail("user@abv.bg");
+
+        User user = User.builder()
+                .id(UUID.randomUUID())
+                .username(dto.getUsername())
+                .email(dto.getEmail())
+                .firstName(dto.getFirstName())
+                .password(dto.getPassword())
+                .build();
+
+
+        lenient().when(userRepository.findByUsernameOrEmail(dto.getUsername(), dto.getEmail())).thenReturn(Optional.empty());
+        lenient().when(userRepository.count()).thenReturn(1L);
+        lenient().when(userRepository.save(any())).thenReturn(user);
+
+        userService.register(dto);
+
+        verify(userRepository, times(1)).save(userCaptor.capture());
+        User registeredUser = userCaptor.getValue();
+
+        assertEquals("user", registeredUser.getUsername());
+        assertEquals("User", registeredUser.getFirstName());
+        assertEquals("user@abv.bg", registeredUser.getEmail());
+
+        assertEquals(UserRole.USER, registeredUser.getRole());
+
+        verify(emailService, times(1)).savePreference(registeredUser.getId(), true, registeredUser.getEmail());
+        verify(walletService, times(1)).createWallet(registeredUser);
+    }
+
+
+        @Test
+    void whenEditUserData_thenChangeDetailsAndSaveDatabase() {
+
+        UUID userId = UUID.randomUUID();
+        UserEditRequest dto = UserEditRequest.builder()
+                .firstName("Mimi")
+                .profilePicture("www.image.com")
+                .email("mimi@abv.bg")
+                .username("mimi123")
+                .build();
+
+        User user = User.builder().build();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        userService.editUserData(userId, dto);
+
+        assertEquals("Mimi", user.getFirstName());
+        assertEquals("www.image.com", user.getProfilePicture());
+        assertEquals("mimi@abv.bg", user.getEmail());
+        assertEquals("mimi123", user.getUsername());
+    }
+
+
+    @Test
+    void whenLoadUserByUsername_thenUsernameNotExistException() {
+
+        String username = "user";
+
+        when(userRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        UsernameNotFoundException exception = assertThrows(UsernameNotFoundException.class,
+                () -> userService.loadUserByUsername(username));
+
+        assertEquals("Username does not exist!", exception.getMessage());
+    }
+
+
+    @Test
+    void whenLoadUserByUsername_thenReturnCorrectAuthenticationDetails() {
+
+        String username = "Ani123";
+
+        User user = User.builder()
+                .id(UUID.randomUUID())
+                .password("123123")
+                .role(UserRole.ADMIN)
+                .build();
+
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
+
+        UserDetails authenticationDetails = userService.loadUserByUsername(username);
+
+        assertTrue(authenticationDetails instanceof AuthenticationDetails);
+        AuthenticationDetails result = (AuthenticationDetails) authenticationDetails;
+
+        assertEquals(user.getId(), result.getUserId());
+        assertEquals(user.getPassword(), result.getPassword());
+        assertEquals(user.getRole(), result.getRole());
+    }
+
+
+    @Test
+    void whenGetAllUsers_thenOnlyUsersWithRoleUserReturned() {
+
+        User user1 = User.builder().username("user1").role(UserRole.USER).build();
+        User user2 = User.builder().username("user2").role(UserRole.ADMIN).build();
+        User user3 = User.builder().username("user3").role(UserRole.USER).build();
+        User user4 = User.builder().username("user4").role(UserRole.ADMIN).build();
+
+        when(userRepository.findAll()).thenReturn(Arrays.asList(user1, user2, user3, user4));
+
+        List<User> result = userService.getAllUsers();
+
+        assertEquals(2, result.size());
+        assertTrue(result.contains(user1));
+        assertTrue(result.contains(user3));
+        assertFalse(result.contains(user2));
+        assertFalse(result.contains(user4));
+    }
+
+    @Test
+    void whenGetById_thenIdDoesNotExist() {
+
+        UUID userId = UUID.randomUUID();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        DomainException exception =
+                assertThrows(DomainException.class, () -> userService.getById(userId));
+
+        assertEquals("User with id [%s] does not exist.".formatted(userId),
+                exception.getMessage());
     }
 }
+
+
 
 
 
