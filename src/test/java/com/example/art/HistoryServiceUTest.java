1916ce3f0058a493a6b1a751cbab73b7f99103c3
AniiitaCode <anita_nen4eva@abv.bg>
2025-03-18 20:57:10 +0200

    History Service Testing

---
+++ src/test/java/com/example/art/HistoryServiceUTest.java
@@ -0,0 +1,63 @@
+package com.example.art;
+
+import com.example.art.design.model.Design;
+import com.example.art.history.model.History;
+import com.example.art.history.repository.HistoryRepository;
+import com.example.art.history.service.HistoryService;
+import com.example.art.user.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@EnableScheduling
+public class HistoryServiceUTest {
+
+    @Mock
+    private HistoryRepository historyRepository;
+
+    @InjectMocks
+    private HistoryService historyService;
+
+    @Test
+    void whenDeleteId_thenDeleteHistory() {
+        UUID historyId = UUID.randomUUID();
+
+        historyService.deleteById(historyId);
+
+        verify(historyRepository, times(1)).deleteById(historyId);
+    }
+
+    @Test
+    void whenSaveHistory_thenSuccessSaveHistoryInDatabase() {
+        Design design = mock(Design.class);
+        User user = mock(User.class);
+
+        when(design.getUser()).thenReturn(user);
+
+        historyService.saveInHistory(design);
+
+        ArgumentCaptor<History> historyArgumentCaptor = ArgumentCaptor.forClass(History.class);
+        verify(historyRepository, times(1)).save(historyArgumentCaptor.capture());
+
+        History capturedHistory = historyArgumentCaptor.getValue();
+
+        assertEquals(user, capturedHistory.getUser());
+    }
+
+    @Test
+    void whenDeleteAllHistory_thenEmptyHistoryRepository() {
+        historyService.deleteAllHistory();
+
+        verify(historyRepository, times(1)).deleteAll();
+    }
+}
