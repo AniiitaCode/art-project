78307fed53e3a7d6dde296b86c3dcf6a1c8a1855
AniiitaCode <anita_nen4eva@abv.bg>
2025-03-30 14:03:23 +0300

    Added method

--- src/main/java/com/example/art/transaction/repository/TransactionRepository.java
+++ src/main/java/com/example/art/transaction/repository/TransactionRepository.java
@@ -2,7 +2,6 @@ package com.example.art.transaction.repository;
 
 import com.example.art.transaction.model.Transaction;
 import com.example.art.user.model.User;
-import com.example.art.wallet.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +11,7 @@ import java.util.UUID;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 
-    List<Transaction> findByWallet(Wallet wallet);
-
     void deleteTransactionsByUser(User user);
+
+    List<Transaction> findByUserIdOrderByTransactionDateDesc(UUID userId);
 }
