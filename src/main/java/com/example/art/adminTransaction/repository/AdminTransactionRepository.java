e3fb70ddee8fd0d616d53201026568a67a570f3f
AniiitaCode <anita_nen4eva@abv.bg>
2025-04-01 18:58:43 +0300

    Added findByUsername

--- src/main/java/com/example/art/adminTransaction/repository/AdminTransactionRepository.java
+++ src/main/java/com/example/art/adminTransaction/repository/AdminTransactionRepository.java
@@ -4,9 +4,11 @@ import com.example.art.adminTransaction.model.AdminTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AdminTransactionRepository extends JpaRepository<AdminTransaction, UUID> {
 
+    List<AdminTransaction> findByUsername(String username);
 }
