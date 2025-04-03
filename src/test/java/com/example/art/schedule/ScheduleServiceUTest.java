8c223f422527a9f6d074306c73a420d01d6d5abe
AniiitaCode <anita_nen4eva@abv.bg>
2025-04-04 00:05:21 +0300

    Change schedule unit test

--- src/test/java/com/example/art/schedule/ScheduleServiceUTest.java
+++ src/test/java/com/example/art/schedule/ScheduleServiceUTest.java
@@ -1,7 +1,10 @@
 package com.example.art.schedule;
 
+import com.example.art.history.service.HistoryService;
+import com.example.art.order.model.OrderStatus;
 import com.example.art.order.model.Orders;
 import com.example.art.order.repository.OrderRepository;
+import com.example.art.order.service.OrderService;
 import com.example.art.schedule.service.ScheduleService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +21,12 @@ import static org.mockito.Mockito.*;
 public class ScheduleServiceUTest {
 
     @Mock
+    private OrderService orderService;
+
+    @Mock
+    private HistoryService historyService;
+
+    @Mock
     private OrderRepository orderRepository;
 
     @InjectMocks
@@ -26,18 +35,25 @@ public class ScheduleServiceUTest {
     @Test
     void whenDeleteOrderById_thenDeleteOrder() {
         UUID orderId = UUID.randomUUID();
+        Orders order = mock(Orders.class);
+
+        when(orderService.getOrderById(orderId)).thenReturn(order);
 
         scheduleService.deleteById(orderId);
 
-        verify(orderRepository, times(1)).deleteById(orderId);
+        verify(order).setStatus(OrderStatus.UNACCEPTED);
+        verify(orderRepository).save(order);
+        verify(historyService).updateStatusHistory(order);
     }
 
     @Test
     void whenAcceptOrder_thenAcceptInSchedule() {
-        Orders order = new Orders();
+        Orders order = mock(Orders.class);
 
         scheduleService.acceptOrder(order);
 
-        verify(orderRepository, times(1)).save(order);
+        verify(order).setStatus(OrderStatus.ACCEPTED);
+        verify(orderRepository).save(order);
+        verify(historyService).updateStatusHistory(order);
     }
 }
