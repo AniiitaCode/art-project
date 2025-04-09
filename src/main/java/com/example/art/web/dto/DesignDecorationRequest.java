be87d1a88e3e257a919e261c13682e1b3bc4c134
AniiitaCode <anita_nen4eva@abv.bg>
2025-04-09 20:09:23 +0300

    Added validation

--- src/main/java/com/example/art/web/dto/DesignDecorationRequest.java
+++ src/main/java/com/example/art/web/dto/DesignDecorationRequest.java
@@ -4,6 +4,7 @@ import com.example.art.design.model.DecorationPebbles;
 import com.example.art.design.model.DecorationPicture;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -11,6 +12,7 @@ import lombok.Data;
 public class DesignDecorationRequest {
 
     @Size(min = 2, max = 2, message = "The color number must be 2 digits!")
+    @Pattern(regexp = "^(?:0[1-9]|[1-5][0-9]|60)$", message = "The color number must be a number between 01 and 60!")
     @NotBlank(message = "The color number cannot be empty!")
     private String color;
 
